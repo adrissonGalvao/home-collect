52475445ad0df4c3dbd66f2ac10c156a20c31813
Adrisson Galvao <adrisson36@gmail.com>
2018-02-20 02:32:46 -0400

    fact:user interact mongoDB

---
+++ repository/user.go
@@ -0,0 +1,47 @@
+package repository
+
+import (
+	"home-collect/domain"
+	"log"
+
+	"gopkg.in/mgo.v2/bson"
+
+	mgo "gopkg.in/mgo.v2"
+)
+
+const (
+	DBSERVER = "localhost"
+	DBNAME   = "homeCollect"
+)
+
+type UserRepository struct {
+	database string
+	session  *mgo.Session
+}
+
+func (u *UserRepository) Connect() {
+	session, err := mgo.Dial(DBSERVER)
+	u.database = DBNAME
+	if err != nil {
+		log.Fatal(err)
+	}
+	u.session = session
+}
+func (u *UserRepository) InsertUser(user domain.User) error {
+	sess := u.session.Copy()
+	defer sess.Close()
+
+	err := sess.DB(u.database).C("user").Insert(&user)
+
+	return err
+}
+
+func (u *UserRepository) FindAllUser() ([]domain.User, error) {
+	sess := u.session.Copy()
+	defer sess.Close()
+	var users []domain.User
+
+	err := sess.DB(u.database).C("user").Find(bson.M{}).All(&users)
+	return users, err
+
+}
