3cf05db4549cb7e2ea33d884de5f2ce058bc423e
Adrisson Galvao <adrisson36@gmail.com>
2018-03-01 01:57:54 -0400

    creating test of the sensor

---
+++ tests/sensor_test.go
@@ -0,0 +1,41 @@
+package test
+
+import (
+	"home-collect/domain"
+	"home-collect/service"
+	mock "home-collect/tests/mocks"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/mgo.v2/bson"
+)
+
+func TestVerifyIntegrityUrlSensor(t *testing.T) {
+	sensorRepositoryMock := new(mock.ISensorRepository)
+
+	sensorRepositoryMock.On("FindUrlSensor", "teste").Return(true, nil)
+
+	sensorService := service.SersoService{sensorRepositoryMock}
+
+	resultTest := sensorService.VerifyIntegrityUrlSensor("teste")
+
+	assert.Equal(t, true, resultTest)
+}
+
+func TestGeneratingUrlSensors(t *testing.T) {
+	sensorRepositoryMock := new(mock.ISensorRepository)
+	var sensors []domain.Sensor
+	sensor := domain.Sensor{}
+	sensor.ID = bson.NewObjectId()
+	sensor.Name = "teste"
+	sensor.Token = "teste"
+	sensor.Url = "teste"
+	sensor.User = sensor.ID
+	sensors = append(sensors, sensor)
+	sensorRepositoryMock.On("FindAllSensor").Return(sensors, nil)
+	sensorService := service.SersoService{sensorRepositoryMock}
+	resultTest, err := sensorService.GeneratingUrlSensors()
+	assert.Error(t, err, "GeneratingUrlSensors return error")
+	assert.Equal(t, "/"+sensor.Url, resultTest[0])
+
+}
