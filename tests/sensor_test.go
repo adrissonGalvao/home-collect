71a7b0a3ba947c305c7b5301f1597e075cf90202
Adrisson Galvao <adrisson36@gmail.com>
2018-03-04 23:40:08 -0400

    refactoring test for BDD

--- tests/sensor_test.go
+++ tests/sensor_test.go
@@ -6,11 +6,11 @@ import (
 	mock "home-collect/tests/mocks"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	. "github.com/smartystreets/goconvey/convey"
 	"gopkg.in/mgo.v2/bson"
 )
 
-func TestVerifyIntegrityUrlSensor(t *testing.T) {
+/*func TestVerifyIntegrityUrlSensor(t *testing.T) {
 	sensorRepositoryMock := new(mock.ISensorRepository)
 
 	sensorRepositoryMock.On("FindUrlSensor", "teste").Return(true, nil)
@@ -20,9 +20,55 @@ func TestVerifyIntegrityUrlSensor(t *testing.T) {
 	resultTest := sensorService.VerifyIntegrityUrlSensor("teste")
 
 	assert.Equal(t, true, resultTest)
+}*/
+
+func TestVerifyIntegrityUrlSensor(t *testing.T) {
+	sensorRepositoryMock := new(mock.ISensorRepository)
+
+	sensorRepositoryMock.On("FindUrlSensor", "teste").Return(true, nil)
+
+	sensorService := service.SensorService{sensorRepositoryMock}
+
+	Convey("Sending URL: Teste", t, func() {
+		Convey("Checking integruty of url sent", func() {
+			resultTest := sensorService.VerifyIntegrityUrlSensor("teste")
+			Convey("Validated url", func() {
+				So(resultTest, ShouldEqual, true)
+			})
+		})
+	})
+
 }
 
-func TestGeneratingUrlSensors(t *testing.T) {
+func TestGenerateUrlsSensor(t *testing.T) {
+	sensorRepositoryMock := new(mock.ISensorRepository)
+	Convey("Creating sensor for test", t, func() {
+		var sensors []domain.Sensor
+
+		sensor := domain.Sensor{}
+		sensor.ID = bson.NewObjectId()
+		sensor.Name = "teste"
+		sensor.Token = "teste"
+		sensor.Url = "teste"
+		sensor.User = sensor.ID
+
+		sensors = append(sensors, sensor)
+
+		sensorRepositoryMock.On("FindAllSensor").Return(sensors, nil)
+
+		sensorService := service.SensorService{sensorRepositoryMock}
+		Convey("Generating Url for routes", func() {
+			resultTest, _ := sensorService.GenerateUrlsSensor()
+			Convey("verifying that the URLs were created correctly", func() {
+				Convey("Url gererated correctly", func() {
+					So("/"+sensors[0].Url, ShouldEqual, resultTest[0])
+				})
+			})
+		})
+	})
+}
+
+/*func TestGeneratingUrlSensors(t *testing.T) {
 	sensorRepositoryMock := new(mock.ISensorRepository)
 
 	var sensors []domain.Sensor
@@ -43,4 +89,4 @@ func TestGeneratingUrlSensors(t *testing.T) {
 
 	assert.Equal(t, "/"+sensor.Url, resultTest[0])
 
-}
+}*/
